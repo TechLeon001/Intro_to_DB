a6ddb3757ed8fd8ad3e594ff53a7217c5f67fb35
TechLeon001 <leonardnaibei92@gmail.com>
2025-06-29 23:39:06 +0300

    changes

--- task_5.sql
+++ task_5.sql
@@ -3,3 +3,6 @@
 
 INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
+
+SELECT * FROM customer WHERE customer_id = 1;
