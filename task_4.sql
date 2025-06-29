ec5b8fe95c72e88547fb46e93264e17d125765f1
TechLeon001 <leonardnaibei92@gmail.com>
2025-06-29 21:02:51 +0300

    all files have been added

---
+++ task_4.sql
@@ -0,0 +1,13 @@
+-- Get full description of books table using INFORMATION_SCHEMA
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_KEY, 
+    COLUMN_DEFAULT, 
+    EXTRA 
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = DATABASE() 
+    AND TABLE_NAME = 'books';
